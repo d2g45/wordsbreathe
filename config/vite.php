57d17e6cda5e81628a2ac39b820af7520c4da4d4
Daniel De Guzman <me@danieldeguzman.com>
2024-11-11 01:18:59 -0500

    fixed manifest path

--- config/vite.php
+++ config/vite.php
@@ -4,7 +4,7 @@ use craft\helpers\App;
 
 return [
     'useDevServer' => App::env('CRAFT_ENVIRONMENT') === 'dev',
-    'manifestPath' => Craft::getAlias('@webroot') . '/dist/manifest.json',
+    'manifestPath' => Craft::getAlias('@webroot') . '/dist/.vite/manifest.json',
     'devServerPublic' => Craft::getAlias('@web') . ':3000',
     'serverPublic' => Craft::getAlias('@web') . '/dist/',
     'errorEntry' => 'src/scripts/main.ts',
